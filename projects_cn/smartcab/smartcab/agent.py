bd96e6fe4730d59435471f5653cc22d9f1cb6eed
wangfeiwilliam <wangfeiwilliam@didichuxing.com>
2016-10-25 00:58:12 +0800

    P4

--- projects_cn/smartcab/smartcab/agent.py
+++ projects_cn/smartcab/smartcab/agent.py
@@ -13,7 +13,7 @@ class LearningAgent(Agent):
         # TODO: Initialize any additional variables here
         self.q = {}
 	self.actions=env.valid_actions
-	self.alpha=0.08
+	self.alpha=0.2
 	self.gamma=0.7
 	self.epsilon =0.09 
 	self.action='None'
@@ -30,32 +30,32 @@ class LearningAgent(Agent):
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
-
+	
         # TODO: Update state
 	state=(self.next_waypoint,inputs['light'],inputs['oncoming'],inputs['left'])
 	q = [self.q.get((state, a),0.0) for a in self.actions]
         maxQ = max(q)
 	if random.random() < self.epsilon:
-		minQ = min(q); mag = max(abs(minQ), abs(maxQ))
-            	q = [q[i] + random.random() * mag - .5 * mag for i in range(len(self.actions))] # add random values to all the actions, recalculate maxQ
-        	maxQ = max(q)
-
-	count = q.count(maxQ)
-       	if count > 1:
-            	best = [i for i in range(len(self.actions)) if q[i] == maxQ]
-            	i = random.choice(best)
-        else:
-            	i = q.index(maxQ)
-	action = self.actions[i]
+		action=random.choice(self.actions)
+	else:
+		count = q.count(maxQ)
+       		if count > 1:
+            		best = [i for i in range(len(self.actions)) if q[i] == maxQ]
+            		i = random.choice(best)
+        	else:
+            		i = q.index(maxQ)
+		action = self.actions[i]
 	reward=self.env.act(self,action)
+
 	if self.state is not None:
 		self.q[(self.state,self.action)]=(1-self.alpha)*self.q.get((self.state,self.action),0.0)+self.alpha*(reward+self.gamma*max([self.q.get((state,a),0.0) for a in self.actions]))
-		
+
+	inputs = self.env.sense(self)
 	self.action = action
-	self.state = state
+	self.state = (self.next_waypoint,inputs['light'],inputs['oncoming'],inputs['left'])
 	if self.env.done :
 		self.sucess+=1	
-        #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 	if deadline<=0 or self.env.done:
 		print self.sucess,self.testtimes
 
